0842c71518baa2d0f8b51759e36dee0e7301be65
Aristidescosta <aristidiscosta200@gmail.com>
2024-08-10 12:15:43 +0100

    feat: adicionando validação do body, query, params e headers com yup

--- src/server/controller/tasks/Create.ts
+++ src/server/controller/tasks/Create.ts
@@ -1,14 +1,34 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
 interface ITask {
   title: string;
   description: string;
   isCompleted: boolean;
-  color: string
+  color: string;
+
 }
 
-export const create = (req: Request<{}, {}, ITask>, res: Response) => {
-  console.log(req.body);
-  return res.status(StatusCodes.CREATED).send('Tarefa criada com sucesso');
+const validationSchema: yup.Schema<ITask> = yup.object({
+  title: yup.string().required().min(5),
+  description: yup.string().required().max(255),
+  isCompleted: yup.boolean().required(),
+  color: yup.string().required().min(3).max(255),
+});
+export const create = async (req: Request<{}, {}, ITask>, res: Response) => {
+  try {
+    await validationSchema.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message
+      }
+    });
+  }
+  const { title } = req.body;
+
+  return res.status(StatusCodes.CREATED).send(`Tarefa "${title}" criada com sucesso`);
 };
