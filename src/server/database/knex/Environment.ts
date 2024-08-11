87ba487be0c366c0569fc4e497ae1bcf10b3ea32
Aristidescosta <aristidiscosta200@gmail.com>
2024-08-11 19:57:07 +0100

    feat: configurando o postgreSQL

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -28,6 +28,20 @@ export const test: Knex.Config = {
   connection: ':memory'
 };
 
-export const production = {
-  ...development
+export const production: Knex.Config = {
+  client: process.env.DATABASE_CLIENT,
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations')
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds')
+  },
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_DATABASE,
+    password: process.env.DATABASE_PASSWORD,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    ssl: { rejectUnauthorized: false }
+  }
 };
